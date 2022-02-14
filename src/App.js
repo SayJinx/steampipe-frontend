30a013dc70c120966047d114dd86af1484fe11fb
SayJinx <whatsupguest@gmail.com>
2022-02-14 13:27:29 -0600

    Began work on landing page, added folders files for basic components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App" >
+      <header className="App-header" >
+
+        <p class="p1">This is a Steampipe Landing Page</p>
+
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,8 +22,21 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+
+      <Grid container spacing={4}>
+          <Grid item xs={12} sm={12} md={2}>
+            <Paper>Hey There</Paper>
+          </Grid>
+          <Grid item xs={12} sm={12} md={2}>
+            <Paper>Hi There</Paper>
+         </Grid>
+          <Grid item xs={12} sm={12} md={2}>
+            <Paper>Ho There</Paper>
+          </Grid>
+        </Grid>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App id="grad" />
   </React.StrictMode>,
   document.getElementById('root')
 );
